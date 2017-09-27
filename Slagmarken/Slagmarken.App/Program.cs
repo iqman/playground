69b907f20c156b78eb2dff7f6a1059186a0c615d
iqman <2522115+iqman@users.noreply.github.com>
2017-09-27 21:37:47 +0200

    test

---
+++ Slagmarken/Slagmarken.App/Program.cs
@@ -0,0 +1,16 @@
+﻿using Slagmarken.Common;
+using System;
+using static System.Console;
+
+
+namespace Slagmarken.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hello = HelloSayer.GiveMeAHello();
+            WriteLine(hello);
+        }
+    }
+}
---
+++ Slagmarken/Slagmarken.Common/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Slagmarken.Common
+{
+    public class HelloSayer
+    {
+        static string HelloText { get; } = "Hello World";
+
+        public static string GiveMeAHello () => HelloText;
+    }
+}
